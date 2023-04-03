6ad48361fa3ebc1105283f054b9c0da9083be693
Moriones20 <moriones06082001@gmail.com>
2023-04-03 08:16:35 -0500

    refactor src-components and apps.js

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,23 @@
-import Card from './Card';
+import Card from "./Card";
 
-export default function Cards(props) {
-   return (
-      <div>
-         {
-            props.characters.map(character => {
-               return (
-                  <div>
-                     {Card(character)}
-                  </div>
-               )
-            }) 
-         }
-      </div>
-   )
+export default function Cards({ characters }) {
+  return (
+    <div>
+      {characters.map((character) => {
+        return(
+         <Card
+            key={character.id}
+            id={character.id}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+            image={character.image}
+            origin={character.origin}
+            onClose={() => window.alert('Emulamos que se cierra la card')}
+         />
+        ) 
+      })}
+    </div>
+  );
 }

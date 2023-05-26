f794f18d07cdee7ed24357aca855b8ac69585b66
Felipe Moriones <moriones06082001@gmail.com>
2023-05-25 19:48:29 -0500

    So

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,7 +3,7 @@ const server = express();
 const router = require("./routes/index");
 
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://rick-and-morty-rose-psi.vercel.app/");
+  res.header("Access-Control-Allow-Origin", "https://rick-and-morty-rose-psi.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");

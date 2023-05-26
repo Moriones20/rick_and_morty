2aad9919072f85b02793da9585031c1a4ab34567
Felipe Moriones <moriones06082001@gmail.com>
2023-05-25 20:00:47 -0500

    Ready for deploy

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const FavoriteModel = require("./models/Favorite");
 const UserModel = require("./models/User");
 
@@ -9,7 +9,7 @@ const UserModel = require("./models/User");
 // Recuerda pasarle la información de tu archivo '.env'.
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   { logging: false, native: false }
 );
 
